785ac80bf7b48fa69797a619dd3149cbc2bddde9
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 17:19:16 -0400

    conditionals_personal commit 4
    This is my 4th commit for assignment conditonal_personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,16 +1,12 @@
 // Kenneth Simpson April 17, 2014 
 //Conditionals_Personal 
 
-/*In this code I am going to calculate the actual amount of sleep a person get as
+/*In this code I am going to calculate the actual amount of sleep a person gets as
 opposed to how much their body needs to get.*/
 
-/*Here I am declaring the time that is the average amount of sleep a human gets
-a night.*/
-var sleepTimeNightly = 8;
-
-/*Here I defining what the average amount of sleep a human needs on a weekly
-basis.*/
-var sleepNeededWeekly = 56;
+/*Here I am prompting the user askting them how many hours of sleep
+do they get a night?*/
+var sleepTimeNightly = prompt("How many hours a night to you sleep?")
 
 
 
