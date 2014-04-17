cf590728f192f26b1538d33b909691203171ec9d
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 17:07:58 -0400

    Changed givens for conditonals_personal assignment
    This is my first commit for conditionals assignment for my personal
    version.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,26 +1,13 @@
 // Kenneth Simpson April 17, 2014 
 //Conditionals_Personal 
 
-/*In this code I am going to calulate how much time I spend working on homework
-in a given week.*/
+/*In this code I am going to calculate the actual amount of sleep a person get as
+opposed to how much their body needs to get.*/
+
+/*Here I am declaring the time that is the average amount of sleep a human gets
+a night.*/
+var sleepTime = 8;
 
 
-/*Here I am declaring a variable and using a prompt box to ask the user how
-many hourse they spend a week on homework!*/
-var hoursSpent = prompt("How many hours a week do you spend on your assignments?")
-//I am checking to make sure my prompt box is working correctly.
-console.log(hoursSpent);
 
-/* I am creating this variable to prompt the user to ask how many days a week do they
-work on their homework.*/
-var spentDays = prompt("How many days a week do you work on your assignments?")
-
-//Here I am checking my prompt box for my variable daysSpent
-console.log(spentDays);
-
-/* Here I am using an if statement to prompt the user if the hours they spend
-on their homework is less than 25 hours.*/
-if (hoursSpent < 25){
-	alert("You need to spend more time on your homework!");
-}
 
