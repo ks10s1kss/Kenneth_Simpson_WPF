dc13f31813afef9524e4d6c13f768274de025bf5
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 17:27:48 -0400

    Commit 7
    This is my 7th commit for conditionals_personal assignment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -11,9 +11,19 @@ var sleepTimeNightly = prompt("How many hours a night to you sleep?");
 //Here I am making sure that my promp box is working correctly within my program!
 console.log(sleepTimeNightly);
 
+/*Now I am going to set my if statement so that I can tell my user whether they
+are getting enough nightly sleep or not.Then I am going to set an else statement
+to let them know they are doing a fine job at getting enough sleep if they are above
+8 hours a night.*/
+if (sleepTimeNightly < 8){
+	alert("You Need to get more sleep!")
+}else{
+	alert("You are getting your much needed hours of sleep!")
+}
+
 /*for this variable I am going to prompt the user and see how many hours of sleep
 they get weekly*/
+var sleepTimeWeekly = prompt("How many hours a night do you get weekly?");
 
-var sleepTimeWeekly = prompt("How many hours a night do you get weekly?")
-
-
+//I am checking to mkae sure my prompt is retaining data correctly
+console.log(sleepTimeWeekly);
