178343ec907e192c030b69a1a3e54899c8aa23ac
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 18:34:14 -0400

    commit 21
    This is my 21st commit for conditionals_wacky assignment

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -10,3 +10,7 @@ var limbNumber = parseInt(prompt("How many limbs are on your tree?"));
 /*I want to make sure that my prompt is collecting the correct data given 
 by the user.*/
 console.log(limbNumber);
+
+/*I am going to use a ternary to tell the user if the amount of limbs they
+entered is even in the realm of possibilites.*/
+(limbNumber >=1,000,000,000) ? alert("You have spent too much time counting tree limbs!") : alert("I bet that took concentration!")
