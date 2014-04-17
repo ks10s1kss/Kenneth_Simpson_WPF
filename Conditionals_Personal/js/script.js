0b1c29699dae60fd276be0ca0da947cd3a0b4a40
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 17:10:52 -0400

    2nd Commit
    This is my 2nd commit for conditionals assignment personal version

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,8 +6,11 @@ opposed to how much their body needs to get.*/
 
 /*Here I am declaring the time that is the average amount of sleep a human gets
 a night.*/
-var sleepTime = 8;
+var sleepTimeNightly = 8;
 
+/*Here I defining what the average amount of sleep a human needs on a weekly
+basis.*/
+var sleepNeededWeekly = 56;
 
 
 
