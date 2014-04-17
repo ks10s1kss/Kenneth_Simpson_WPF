a5380fe3d96305edb5017835bb816cba48aec14e
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 18:24:39 -0400

    Commit 19
    This is my 19th commit to conditionals_wacky assignment

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,4 +1,9 @@
-// JavaScript
+//Kenneth Simpson April 17, 2014 
+//Conditionals_Wacky
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+/*In this program I am going to calculates the number of leaves that are on a tree.
+
+/*Here I am creating a variable to prompt the user to answer how many limbs
+are on the tree they are wondering about.*/
+var limbNumber = parseInt(prompt("How many limbs are on your tree?"));
+
