5fcc486916026b80564b9243123c17304bb8e070
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-17 16:38:25 -0400

    conditionals_personal assignment
    This is my second commit for my conditionals_personal assignment.

--- Conditionals_Personal/js/scirpt.js
+++ Conditionals_Personal/js/scirpt.js
@@ -7,4 +7,6 @@ in a given week.*/
 
 /*Here I am declaring a variable and using a prompt box to ask the user how
 many hourse they spend a week on homework!*/
-var hoursSpent = prompt("How many hourse a week do you spend on your assignments?
+var hoursSpent = prompt("How many hourse a week do you spend on your assignments?")
+//I am checking to make sure my prompt box is working correctly.
+console.log(hoursSpent);
