19249f30df3c96c9e994ae6deec018265e18b741
Kenneth Simpson <simpks01@fullsail.edu>
2014-04-30 20:50:30 -0400

    Functions Worksheet
    2nd commit for Functions_Worksheet week 4

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,5 +1,7 @@
 //Kenneth Simpson 4/30/2014 Functions_Worksheet
 
-//Here I am going to declare the variables for the radius.
+//Here I am going to declare the variable for the radius.
 var radius = 4;
-var radius = 6;
+
+//Here I am going to declare the second variable pie.
+var pie = 3.14;
