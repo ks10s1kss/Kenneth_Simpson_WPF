02068877f12a879ebcd21bfd8f87fe864b7cd8b5
Kenneth Simpson <simpks01@fullsail.edu>
2014-05-01 10:16:35 -0400

    Functions Worksheet
    16th commit for Functions_Worksheet for week 4

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,5 +1,11 @@
 //Kenneth Simpson 4/30/2014 Functions_Worksheet
 
+//I am declaring the 2x part of the circumference equation with the variable two.
+var two = 2;
+
+var pie = 3.14;
+
+var radius = 6;
 /*Here I am passing arguments 2, 3.14, 6, to my parameters so that they can be used.
 I am also setting up the var total so that I can use console.log to print out the answer.*/
 var total = calcCircumference(2, 3.14, 6);
