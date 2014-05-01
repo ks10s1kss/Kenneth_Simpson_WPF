043b13b371601621964ac3f31fff9e02ac6cc25f
Kenneth Simpson <simpks01@fullsail.edu>
2014-05-01 10:11:01 -0400

    Functions Worksheet
    12th commit to Functions_Worksheet for week 4

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -19,6 +19,8 @@ console.log("The circumference of the circle is " + total);
 //Here I am declaring the victims weight. 
 var vicWeight = 165;
 
+//I am passing arguments to the parameters and also declaring the variable total
+//so that I can have somewhere to print out my results. 
 var total = calcNumberOfStings(165, 8.666666667);
 
 function calcNumberOfStings(vicWeight, stings){
