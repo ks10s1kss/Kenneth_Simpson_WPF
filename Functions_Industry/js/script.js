e8ebea09c830e22e460c7fcefd4f591400b54a15
Kenneth Simpson <simpks01@fullsail.edu>
2014-05-01 18:05:48 -0400

    Functions Industry
    13th commit for Functions_Industry assignment for week 4 of WPF

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -15,18 +15,19 @@ console.log(bolts);
 (bolts < 10) ? alert("You seriously need to pick it up!") : alert("You are working at a good pace!");
 
 
-
+//I am defining the variable to prompt the user for information about their work day. 
 var trigAssemblies = parseInt(prompt("How many trigger assemblies can you detail strip in an hour?"));
-
+//I am priniting to the console to make sure th information entered is correct.
 console.log(trigAssemblies);
 
 
-
+// I am using an if statement to tell the program that if the user does not enter a value
+//for the trigAssemblies prompt box to create another prompt box prompting them to enter a value. 
 if (trigAssemblies == " ") {
 	var trigAssemblies = prompt("You forgot to input a value!");
 }
 
-
+//I am declaring my final variable to promp the user for information about the variable.
 var barrels = parseInt(prompt("How many barrels can you detail strip in an hour?"));
 
 console.log(barrels);
